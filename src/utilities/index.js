1a82ec57bb49cc23cf258f9256fee2cf9e89e803
namadi <nmuawiyah@gmail.com>
2018-12-18 16:06:32 +0100

    utilities cleaned

--- src/utilities/index.js
+++ src/utilities/index.js
@@ -1,7 +1,5 @@
 import formatDate from './formatDate';
 import timeSince from './timeSince';
-import transactionToType from './transactionToType';
 import storage from './storage';
-import removeArrayDuplicates from './removeArrayDuplicates';
 
-export { formatDate, timeSince, transactionToType, storage, removeArrayDuplicates};
+export { formatDate, timeSince, storage};
