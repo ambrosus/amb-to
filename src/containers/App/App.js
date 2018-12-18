0e2115f7f2f27d681984277ab63662f668ce3554
namadi <nmuawiyah@gmail.com>
2018-12-18 16:53:41 +0100

    header added

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 import logo from '../../assets/images/logo.svg';
 import './App.scss';
 import Search from '../Search';
+import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* <header className="App-header">
-        </header> */}
+        <Header/>
         <Search/>
         <Footer/>
       </div>
