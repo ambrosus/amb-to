4d798da8fca0b958a031a78ba80e626fc6e57714
namadi <nmuawiyah@gmail.com>
2019-01-09 08:08:44 +0100

    helper functions refactor

---
+++ src/app/utils/helpers.ts
@@ -0,0 +1,22 @@
+const toggleMenu = (event: any) => {
+    event.preventDefault();
+    const parent = event.currentTarget.parentElement;
+    parent.classList.toggle('menu--active');
+};
+
+const valueJSON = (value: any) => {
+    return value.replace(/["{}\[\]]/g, '').replace(/^\s+/m, '');
+}
+
+const isObject = (value: any) => {
+    if ((typeof value === "object") && (value !== null)) {
+        return true;
+    }
+    return false;
+}
+
+export {
+    toggleMenu,
+    valueJSON,
+    isObject
+}
--- src/app/utils/timeSince.ts
+++ src/app/utils/timeSince.ts
@@ -1,14 +1,14 @@
 /**
- * Function to convert a unix timestamp into "time ago" format.
+ * Convert unix timestamp into "time ago" format.
  *
  * @param {number} date - Unix timestamp
  *
  * @returns {string}
  */
 
-export default function timeSince(date) {
+export default function timeSince(date: number) {
 
-  let seconds = Math.floor((new Date() - date) / 1000);
+  let seconds = Math.floor((+new Date() - date) / 1000);
   let interval = Math.floor(seconds / 31536000);
 
   if (interval >= 1) {
@@ -30,5 +30,8 @@ export default function timeSince(date) {
   if (interval >= 1) {
     return interval + " minute" + (interval > 1 ? 's' : '');
   }
-  return Math.floor(seconds) + " second" + (seconds > 1 ? 's' : '');
+
+  seconds = seconds < 1 ? 1 : seconds;
+
+  return Math.floor(seconds) + " second" + (seconds !== 1 ? 's' : '');
 }
