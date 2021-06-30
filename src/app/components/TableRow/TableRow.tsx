9b4d2bf3d1e06148641b08c9bb89aa113640282a
motzart <8etaj@ukr.net>
2021-06-30 20:45:37 +0300

    add feature

--- src/app/components/TableRow/TableRow.tsx
+++ src/app/components/TableRow/TableRow.tsx
@@ -2,8 +2,9 @@
  * Copyright 2018 Ambrosus Inc.
  * Email: tech@ambrosus.com
  */
-import React, { SFC } from 'react';
+import React, {SFC, useEffect, useState} from 'react';
 import { getStyles, generateLink } from '../../utils';
+import { toJS } from 'mobx';
 
 interface TableRowProps {
   title: any;
@@ -18,12 +19,74 @@ const TableRow: SFC<TableRowProps> = ({ title, value, titleClass = [],
   const rClass = ['table-row', ...rowClass].join(' ').trim();
   const tClass = ['table-cell--title', ...titleClass].join(' ').trim();
   const cClass = ['table-cell', ...cellClass].join(' ').trim();
+  const [isShowing,setIsShowing]=useState(false);
+  const [currentImg,setCurrentImg]=useState('');
+  const checkTypeData= (data:any): any => {
+    if(typeof data ==='string'){
+      return generateLink(data)
+    }else if(data.length>0){
+      const newValue = toJS(data)
+      return (
+          <div className='table__raws'>{newValue.map(el=>{
+            const fileBase64 = el.data;
+            const body = fileBase64.split(',')[1];
+            // Get content type from base64 string
+            const contentType = fileBase64.match(/[^:]\w+\/[\w-+\d.]+(?=;|,)/)[0];
+            if(contentType==="application/pdf"){
+              return <a  target="_blank" href="#" className='' onClick={(e)=>handleClick(e,body, contentType)}>{el.name}</a>
+            }else {
+              return<div>
+                <img className='table__gallery-main' onClick={()=> {
+                  setCurrentImg(fileBase64);
+                  setIsShowing(true);
+                }} src={fileBase64} alt="img"/>
+                {isShowing && <div className="table__gallery-scale">
+                  <div className="table__gallery-wrap"><img src={currentImg} alt=""/></div>
+                  <div className="table__gallery-bg" onClick={()=>setIsShowing(!isShowing)}></div>
+                </div>}
+              </div>
+              }})}
+          </div>
+      );
+
+    }
+  }
+
+  const handleClick = (e,body, contentType) => {
+    e.preventDefault();
+    const blob = b64toBlob(body, contentType);
+    const blobUrl = URL.createObjectURL(blob);
+    window.open(blobUrl, '_blank');
+  }
+  const b64toBlob = (b64Data, contentType='', sliceSize=512) => {
+    const byteCharacters = atob(b64Data);
+    const byteArrays: Array<any> = [];
+
+    for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+      const slice = byteCharacters.slice(offset, offset + sliceSize);
+
+      const byteNumbers = new Array(slice.length);
+      for (let i = 0; i < slice.length; i++) {
+        byteNumbers[i] = slice.charCodeAt(i);
+      }
+
+      const byteArray = new Uint8Array(byteNumbers);
+      byteArrays.push(byteArray);
+    }
+
+    const blob = new Blob(byteArrays, {type: contentType});
+    return blob;
+  }
+
+  const val = checkTypeData(value) ;
   return (
     <div className={rClass}>
       <div className={tClass} style={getStyles('components_keys')}>{title}</div>
       <div className={cClass} style={getStyles('components_values')}
-        dangerouslySetInnerHTML={{ __html: generateLink(value) }}>
+        >
+        {val}
       </div>
+
     </div >
   );
 };
--- src/app/containers/Event/components/Details/Details.tsx
+++ src/app/containers/Event/components/Details/Details.tsx
@@ -2,16 +2,85 @@
  * Copyright 2018 Ambrosus Inc.
  * Email: tech@ambrosus.com
  */
-import React, { SFC, Fragment } from 'react';
+import React, {SFC, Fragment, useState, useEffect} from 'react';
 import './Details.scss';
 import Item from '../../../../components/Item';
 import TableRow from '../../../../components/TableRow';
-import { loopExclude, getStyles, valueJSON, formatDate, isObject } from '../../../../utils';
+import {loopExclude, getStyles, valueJSON, formatDate, isObject, generateLink} from '../../../../utils';
+import { toJS } from 'mobx'
 
 interface DetailsProps {
   event: any;
 }
 const Details: SFC<DetailsProps> = ({ event }) => {
+  const [arrImg,setArrImg]=useState([]);
+  const [isShowing,setIsShowing]=useState(false);
+  const [currentImg,setCurrentImg]=useState('');
+  const checkArrayByType = () =>{
+    if(toJS(event).raws.length>0){
+      setArrImg(toJS(event).raws.filter(el=>{
+        if(el.type === "image/png")return el
+      }))
+    }
+  }
+  const checkTypeData= (data:any): any => {
+  if(toJS(data).length>0){
+      const newValue = toJS(data)
+      return (
+          <div className='table__raws-det'>{newValue.map(el=>{
+            const fileBase64 = el.data;
+            const body = fileBase64.split(',')[1];
+            // Get content type from base64 string
+            const contentType = fileBase64.match(/[^:]\w+\/[\w-+\d.]+(?=;|,)/)[0];
+            if(contentType==="application/pdf"){
+              return <a   target="_blank" href="#" className='' onClick={(e)=>handleClick(e,body, contentType)}>{el.name}</a>
+            }else {
+              return<div>
+                <img className='table__gallery-main' onClick={()=> {
+                  setCurrentImg(fileBase64);
+                  setIsShowing(true);
+                }} src={fileBase64} alt="img"/>
+                {isShowing && <div className="table__gallery-scale">
+                  <div className="table__gallery-wrap"><img src={currentImg} alt=""/></div>
+                  <div className="table__gallery-bg" onClick={()=>setIsShowing(!isShowing)}></div>
+                </div>}
+              </div>
+            }})}
+          </div>
+      );
+
+    }
+  }
+
+  const handleClick = (e,body, contentType) => {
+    e.preventDefault();
+    const blob = b64toBlob(body, contentType);
+    const blobUrl = URL.createObjectURL(blob);
+    window.open(blobUrl, '_blank');
+  }
+  const b64toBlob = (b64Data, contentType='', sliceSize=512) => {
+    const byteCharacters = atob(b64Data);
+    const byteArrays: Array<any> = [];
+
+    for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+      const slice = byteCharacters.slice(offset, offset + sliceSize);
+
+      const byteNumbers = new Array(slice.length);
+      for (let i = 0; i < slice.length; i++) {
+        byteNumbers[i] = slice.charCodeAt(i);
+      }
+
+      const byteArray = new Uint8Array(byteNumbers);
+      byteArrays.push(byteArray);
+    }
+
+    const blob = new Blob(byteArrays, {type: contentType});
+    return blob;
+  }
+  // useEffect(()=>{
+  //   checkTypeData(event.raws)
+  //
+  // },[])
   return (
     <Item title='Event Details'>
       <Fragment>
@@ -30,6 +99,23 @@ const Details: SFC<DetailsProps> = ({ event }) => {
               ));
           })}
         </div>
+        {event.raws.length>0 &&
+          <Item title='Files Details'>
+          {/*  <div className=' table table__gallery'>*/}
+          {/*{arrImg.map(el=> <div>*/}
+          {/*  <img className='table__gallery-main' onClick={()=> {*/}
+          {/*    setCurrentImg(el['data']);*/}
+          {/*    setIsShowing(true);*/}
+          {/*  }} src={el['data']} alt="img"/>*/}
+          {/*  {isShowing && <div className="table__gallery-scale">*/}
+          {/*    <div className="table__gallery-wrap"><img src={currentImg} alt=""/></div>*/}
+          {/*    <div className="table__gallery-bg" onClick={()=>setIsShowing(!isShowing)}></div>*/}
+          {/*  </div>}*/}
+          {/*</div>)}*/}
+          {/*  </div>*/}
+            {checkTypeData(event.raws)}
+           </Item>
+        }
         {
           loopExclude(event, ['location', 'eventId', 'type', 'documents'])
             .map(([key, value]) => {
