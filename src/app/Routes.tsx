966e5ae887b23a512cd3fb7c17aded2069dbd4dc
melnicenkovadik <melnicenkovadik@gmail.com>
2021-07-07 10:51:06 +0300

    Add loaders to load the asset / event
    #AMB-97 #1h

--- src/app/Routes.tsx
+++ src/app/Routes.tsx
@@ -2,21 +2,24 @@
  * Copyright 2018 Ambrosus Inc.
  * Email: tech@ambrosus.com
  */
-import React, { lazy, ComponentType } from 'react';
-import { Switch, Route, Redirect } from 'react-router';
-import { lazyLoad, retry } from './utils';
+import React, {lazy, Suspense} from 'react';
+import {Redirect, Route, Switch} from 'react-router';
+import {lazyLoad, retry} from './utils';
+import Loader from "./components/Loader";
 
 const Home: any = lazy(() => retry(() => import('./containers/Home')));
 const AssetWrapper: any = lazy(() => retry(() => import('./containers/AssetWrapper')));
 
 const Routes = () => {
-    return (
-        <Switch>
-            <Route path='/:assetId' component={lazyLoad(AssetWrapper)} />
-            <Route exact path='/' component={lazyLoad(Home)} />
-            <Redirect from='*' to='/' />
-        </Switch>
-    );
+  return (
+    <Switch>
+      <Suspense fallback={<Loader/>}>
+        <Route path='/:assetId' component={lazyLoad(AssetWrapper)}/>
+        <Route exact path='/' component={lazyLoad(Home)}/>
+        <Redirect from='*' to='/'/>
+      </Suspense>
+    </Switch>
+  );
 };
 
 export default Routes;
